60dc255f7074f4bfcb19ab5dc899b8c485749015
vinicius <viniciusolvs@proton.me>
2023-10-16 09:56:44 -0300

    add: todo-comments

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -188,8 +188,25 @@ return {
 		"echasnovski/mini.move",
 		version = false,
 		lazy = false,
-		config = function ()
+		config = function()
 			require("mini.move").setup()
 		end
 	},
+
+	-- Finds and lists all of the TODO, HACK, BUG, etc comment in your project and loads them into a browsable list.
+	{
+		"folke/todo-comments.nvim",
+		cmd = { "TodoTrouble", "TodoTelescope" },
+		event = "LazyFile",
+		config = true,
+		-- stylua: ignore
+		keys = {
+			{ "]t",         function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+			{ "[t",         function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+			{ "<leader>xt", "<cmd>TodoTrouble<cr>",                              desc = "Todo (Trouble)" },
+			{ "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>",      desc = "Todo/Fix/Fixme (Trouble)" },
+			{ "<leader>st", "<cmd>TodoTelescope<cr>",                            desc = "Todo" },
+			{ "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>",    desc = "Todo/Fix/Fixme" },
+		},
+	}
 }
