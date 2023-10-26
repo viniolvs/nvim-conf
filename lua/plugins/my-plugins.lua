4fd13cf7cce12bde00f3cb74447ca6a7eb6f881a
vinicius <viniciusolvs@proton.me>
2023-10-26 14:10:05 -0300

    disabled codeium tab completion

--- lua/plugins/my-plugins.lua
+++ lua/plugins/my-plugins.lua
@@ -64,6 +64,7 @@ return {
     "Exafunction/codeium.vim",
     event = "BufEnter",
     config = function()
+      vim.g.codeium_no_map_tab = 1
       -- Change '<C-g>' here to any keycode you like.
       vim.keymap.set("i", "<C-a>", function()
         return vim.fn["codeium#Accept"]()
