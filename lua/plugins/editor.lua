e62a1d09e97a1e75df3baa2e705d861f642838b3
Vinicius de Oliveira <viniolvs.dev@gmail.com>
2024-11-08 17:41:26 -0300

    fix Trouble nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -200,8 +200,7 @@ return {
 
 	{
 		"folke/trouble.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		lazy = false,
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
 		cmd = "Trouble",
 		keys = {
 			{
@@ -213,6 +212,21 @@ return {
 				"<leader>xX",
 				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
 				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
 			},
 			{
 				"<leader>xQ",
@@ -232,8 +246,6 @@ return {
 		keys = {
 			{ "]t",         function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
 			{ "[t",         function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-			{ "<leader>xt", "<cmd>TodoTrouble<cr>",                              desc = "Todo (Trouble)" },
-			{ "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>",      desc = "Todo/Fix/Fixme (Trouble)" },
 			{ "<leader>st", "<cmd>TodoTelescope<cr>",                            desc = "Todo" },
 			{ "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>",    desc = "Todo/Fix/Fixme" },
 		},
