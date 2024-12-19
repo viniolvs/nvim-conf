b1369b4d00d4df3c2366dd4a323d3070d6d5ad86
Vinicius de Oliveira <viniolvs.dev@gmail.com>
2024-12-19 10:47:16 -0300

    add: cursor line on active buffer

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,22 @@
-local Terminal  = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true })
 
 function Lazygit_toggle()
   lazygit:toggle()
 end
+
+-- Ativa cursorline ao entrar no buffer
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*", -- Aplica a todos os buffers
+  callback = function()
+    vim.opt_local.cursorline = true
+  end,
+})
+
+-- Desativa cursorline ao sair do buffer
+vim.api.nvim_create_autocmd("BufLeave", {
+  pattern = "*", -- Aplica a todos os buffers
+  callback = function()
+    vim.opt_local.cursorline = false
+  end,
+})
