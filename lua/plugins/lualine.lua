a7f45f5ad55987643d2a7e9eea3ed7dcbbc9f284
Vinicius de Oliveira <viniolvs.dev@gmail.com>
2025-03-19 16:08:11 -0300

    fix lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -20,12 +20,11 @@ return {
 					lualine_b = {
 						"branch",
 						"diff",
-						"diagnostics",
 						{
 							'filename',
 							file_status = true, -- Displays file status (readonly status, modified status)
 							newfile_status = true, -- Display new file status (new file means no write after created)
-							path = 1,-- 0: Just the filename
+							path = 0,       -- 0: Just the filename
 							-- 1: Relative path
 							-- 2: Absolute path
 							-- 3: Absolute path, with tilde as the home directory
@@ -40,6 +39,7 @@ return {
 								newfile = '+', -- Text to show for newly created file before first write
 							}
 						},
+						"diagnostics",
 					},
 					lualine_c = {},
 					lualine_x = { "encoding", "fileformat", "filetype" },
