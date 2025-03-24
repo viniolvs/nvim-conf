cdaddbbc3aab8be2271202ee46cd3f0a0e459452
Vinicius de Oliveira <viniolvs.dev@gmail.com>
2025-03-24 09:10:57 -0300

    rest keymap

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -7,6 +7,17 @@ return {
 				opts.ensure_installed = opts.ensure_installed or {}
 				table.insert(opts.ensure_installed, "http")
 			end,
-		}
+		},
+
+		config = function()
+			-- first load extension
+			require("telescope").load_extension("rest")
+
+			-- keymaps
+			-- Telescope rest select_env
+			vim.keymap.set("n", "<C-e>", function()
+				return require("telescope").extensions.rest.select_env()
+			end, { desc = "Rest: Select environment" })
+		end,
 	}
 }
